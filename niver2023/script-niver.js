b7830fca03460b56162e02da3e416bc6f7fd31e3
Patrick Bonnereau <artepatrick@gmail.com>
2023-06-07 18:40:42 -0300

    Organizando todo o CSS

--- niver2023/script-niver.js
+++ niver2023/script-niver.js
@@ -33,3 +33,46 @@ form.addEventListener("submit", async (event) => {
       // You can display an error message to the user or take any other appropriate action
     });
 });
+
+function getTimeRemaining(endTime) {
+  const total = Date.parse(endTime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds
+  };
+}
+
+function initializeClock(endTime) {
+  const daysSpan = document.getElementById('days');
+  const hoursSpan = document.getElementById('hours');
+  const minutesSpan = document.getElementById('minutes');
+  const secondsSpan = document.getElementById('seconds');
+
+  function updateClock() {
+    const timeRemaining = getTimeRemaining(endTime);
+
+    daysSpan.textContent = ('0' + timeRemaining.days).slice(-2);
+    hoursSpan.textContent = ('0' + timeRemaining.hours).slice(-2);
+    minutesSpan.textContent = ('0' + timeRemaining.minutes).slice(-2);
+    secondsSpan.textContent = ('0' + timeRemaining.seconds).slice(-2);
+
+    if (timeRemaining.total <= 0) {
+      clearInterval(timeInterval);
+    }
+  }
+
+  updateClock();
+  const timeInterval = setInterval(updateClock, 1000);
+}
+
+const countdownDate = new Date('June 24, 2023 00:00:00');
+initializeClock(countdownDate);
+
