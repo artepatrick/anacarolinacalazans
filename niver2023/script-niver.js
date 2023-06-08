ebaeae8e0ab56837a9a955fe6cfa83d8b9d9071f
Patrick Bonnereau <artepatrick@gmail.com>
2023-06-08 02:18:53 -0300

    Criando Status e iniciando a lógica de trazer os convidados que confirmaram

--- niver2023/script-niver.js
+++ niver2023/script-niver.js
@@ -1,7 +1,12 @@
 const form = document.getElementById("apiForm");
 const button = document.getElementById("button");
 const inputName = document.getElementById("name");
+const inputEmail = document.getElementById("email");
 const statusResponse = document.getElementById("response");
+
+apiForm.addEventListener("click", () => {
+  statusResponse.textContent = "";
+});
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault(); // Prevent the default form submission
@@ -14,6 +19,7 @@ form.addEventListener("submit", async (event) => {
   const ipAddress = await getIPAddress();
   const region = await getRegion();
   const time = new Date();
+  const primeiroNome = getFirstWord(name);
 
   try {
     const envio = await gravaUser(name, email, region, ipAddress, time);
@@ -30,7 +36,16 @@ form.addEventListener("submit", async (event) => {
   button.ariaDisabled = false;
   inputName.value = "";
   inputEmail.value = "";
-  statusResponse.textContent = "Que bom que você vem!";
+  statusResponse.textContent = `Que bom que você vem, ${primeiroNome}!\nveja quem mais vai\n`;
+  try {
+    const data = await getUsers();
+    displayUserNames(data);
+    console.log(`Puxando os registros todos...\n${JSON.stringify(data)}`);
+  } catch (error) {
+    console.log(
+      `A lógica de mostrar todos os registros quebrou...\nerro: ${error}`
+    );
+  }
 });
 
 async function gravaUser(name, email, region, ipAddress, time) {
@@ -54,7 +69,7 @@ async function gravaUser(name, email, region, ipAddress, time) {
   return apiReturn;
 }
 
-async function getRegion() {
+async function getRegion(data) {
   try {
     const response = await fetch("http://ip-api.com/json");
     const data = await response.json();
@@ -117,3 +132,61 @@ function initializeClock(endTime) {
 
 const countdownDate = new Date("June 24, 2023 00:00:00");
 initializeClock(countdownDate);
+
+function getFirstWord(str) {
+  // Trim leading and trailing whitespace
+  const trimmedStr = str.trim();
+
+  // Find the index of the first space character
+  const spaceIndex = trimmedStr.indexOf(" ");
+
+  if (spaceIndex === -1) {
+    // If no space is found, the entire string is considered as the first word
+    return trimmedStr;
+  } else {
+    // Extract the substring from the start of the string till the first space
+    const firstWord = trimmedStr.substring(0, spaceIndex);
+    return firstWord;
+  }
+}
+
+async function getUsers() {
+  const apiReturn = await fetch(
+    "https://artepatrick-mongodb-api.herokuapp.com/niver",
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+}
+
+function displayUserNames(data) {
+  const userList = document.getElementById("userList");
+  console.log(
+    `Veja como o Fetch está trazendo o dado:\n\n${JSON.stringify(data)}`
+  );
+
+  try {
+    for (let i = 0; i < data.length; i++) {
+      const users = data[i];
+
+      // Check if the current item is an array of users
+      if (Array.isArray(users)) {
+        // Iterate over the users array
+        for (let j = 0; j < users.length; j++) {
+          const user = users[j];
+          const userName = user.userName;
+          const listItem = document.createElement("li");
+          listItem.textContent = userName;
+
+          // Append the <li> element to the user list
+          userList.appendChild(listItem);
+        }
+      }
+    }
+  } catch (error) {
+    console.log(
+      `Não deu para tratar a lista dos convidados confirmados...\nErro\n${erro}`
+    );
+  }
+}
