bdc6ddcb3e6eba73d9886edaacf2e2e42b2f1615
Patrick Bonnereau <artepatrick@gmail.com>
2023-06-08 01:00:54 -0300

    Ativado formulário com gravação - em teste

--- niver2023/script-niver.js
+++ niver2023/script-niver.js
@@ -3,36 +3,66 @@ const form = document.getElementById("apiForm");
 form.addEventListener("submit", async (event) => {
   event.preventDefault(); // Prevent the default form submission
 
-  // Get the form data
   const name = document.getElementById("name").value;
-  const presence = document.getElementById("presence").value;
+  const email = document.getElementById("email").value;
+  const ipAddress = await getIPAddress();
+  const region = await getRegion();
+  const time = new Date();
 
-  // Create an object with the form data
-  const formData = {
-    "entry.1234567890": name, // Replace "1234567890" with the actual entry ID from your Google Form
-    "entry.0987654321": presence, // Replace "0987654321" with the actual entry ID from your Google Form
+  try {
+    const envio = await gravaUser(name, email, region, ipAddress, time);
+    if (!envio) {
+      console.log('ATENÇÃO!\nA API não retornou nada no POST do "/User"...');
+    } else {
+      console.log(`Envio realizado par o banco: \n${envio}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+async function gravaUser(name, email, region, ipAddress, time) {
+  const response = {
+    userName: name,
+    email: email,
+    region,
+    ipAddress,
+    time,
   };
 
-  // Send the form data to the Google Form
-  await fetch("https://docs.google.com/forms/d/e/your-form-id/formResponse", {
-    method: "POST",
-    mode: "no-cors",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(function (response) {
-      // Handle the form submission success
-      console.log("Form submitted successfully!");
-      // You can perform any additional actions here, such as showing a success message to the user
-    })
-    .catch(function (error) {
-      // Handle the form submission error
-      console.error("Form submission error:", error);
-      // You can display an error message to the user or take any other appropriate action
-    });
-});
+  const apiReturn = await fetch(
+    "https://artepatrick-mongodb-api.herokuapp.com/niver",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(response),
+    }
+  );
+  console.log(`Response enviado via API:\n${response}`);
+  return apiReturn;
+}
+
+async function getRegion() {
+  try {
+    const response = await fetch("http://ip-api.com/json");
+    const data = await response.json();
+    return data.region;
+  } catch (error) {
+    console.error("Error getting region:", error);
+    return null;
+  }
+}
+
+async function getIPAddress() {
+  try {
+    const response = await fetch("https://api.ipify.org?format=json");
+    const data = await response.json();
+    return data.ip;
+  } catch (error) {
+    console.error("Error getting IP address: ", error);
+    return null;
+  }
+}
 
 function getTimeRemaining(endTime) {
   const total = Date.parse(endTime) - Date.parse(new Date());
@@ -46,23 +76,23 @@ function getTimeRemaining(endTime) {
     days,
     hours,
     minutes,
-    seconds
+    seconds,
   };
 }
 
 function initializeClock(endTime) {
-  const daysSpan = document.getElementById('days');
-  const hoursSpan = document.getElementById('hours');
-  const minutesSpan = document.getElementById('minutes');
-  const secondsSpan = document.getElementById('seconds');
+  const daysSpan = document.getElementById("days");
+  const hoursSpan = document.getElementById("hours");
+  const minutesSpan = document.getElementById("minutes");
+  const secondsSpan = document.getElementById("seconds");
 
   function updateClock() {
     const timeRemaining = getTimeRemaining(endTime);
 
-    daysSpan.textContent = ('0' + timeRemaining.days).slice(-2);
-    hoursSpan.textContent = ('0' + timeRemaining.hours).slice(-2);
-    minutesSpan.textContent = ('0' + timeRemaining.minutes).slice(-2);
-    secondsSpan.textContent = ('0' + timeRemaining.seconds).slice(-2);
+    daysSpan.textContent = ("0" + timeRemaining.days).slice(-2);
+    hoursSpan.textContent = ("0" + timeRemaining.hours).slice(-2);
+    minutesSpan.textContent = ("0" + timeRemaining.minutes).slice(-2);
+    secondsSpan.textContent = ("0" + timeRemaining.seconds).slice(-2);
 
     if (timeRemaining.total <= 0) {
       clearInterval(timeInterval);
@@ -73,6 +103,5 @@ function initializeClock(endTime) {
   const timeInterval = setInterval(updateClock, 1000);
 }
 
-const countdownDate = new Date('June 24, 2023 00:00:00');
+const countdownDate = new Date("June 24, 2023 00:00:00");
 initializeClock(countdownDate);
-
