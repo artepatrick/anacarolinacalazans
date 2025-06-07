3574d5f929cdd85b9b8dc2b42842142b56fa086c
artepatrick <artepatrick@gmail.com>
2025-06-07 13:23:45 -0300

    Remove netlify.toml and related plugin configuration files to streamline project setup

--- niver2025-legado/server/services/spotify/index.js
+++
@@ -1,4 +0,0 @@
-import { spotifyApi, getAuthUrl } from "./spotify-api.js";
-import { addTrackToPlaylist } from "./playlist.js";
-
-export { spotifyApi, getAuthUrl, addTrackToPlaylist };
--- niver2025-legado/src/main.js
+++
@@ -1,8 +0,0 @@
-// Import all necessary modules
-import "../config.js";
-import "./services/api.js";
-import "../script.js";
-import "../music.js";
-
-// Initialize the application
-console.log("Application initialized");
